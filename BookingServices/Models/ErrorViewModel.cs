d794aa663830e9ad2f9e4aa534d13b6f1c7be4e4
Abdelrahman Emad Eldeen Ragab <87204675+AbdoOo20@users.noreply.github.com>
2024-09-20 22:47:58 +0300

    delete files

--- BookingServices/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookingServices.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
