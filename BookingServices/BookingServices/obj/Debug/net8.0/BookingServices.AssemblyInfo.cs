5bb178fa9802c97dd9aa18ae64457ccff57710b8
Abdelrahman Emad Eldeen Ragab <87204675+AbdoOo20@users.noreply.github.com>
2024-09-21 04:45:16 +0300

    createdBranches

--- BookingServices/BookingServices/obj/Debug/net8.0/BookingServices.AssemblyInfo.cs
+++ BookingServices/BookingServices/obj/Debug/net8.0/BookingServices.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f06d87d16043a4ddbec2def54b75f4903fa3a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2af59cd6162226f1ae49070c11067f8c565f033")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
