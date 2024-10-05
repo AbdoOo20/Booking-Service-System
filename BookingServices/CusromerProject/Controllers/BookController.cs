3ac858bde81dc17bf12635f8dc5142b18e4245f1
Abdelrahman Emad Eldeen Ragab <87204675+AbdoOo20@users.noreply.github.com>
2024-10-05 19:19:34 +0300

    book controller api

--- BookingServices/CusromerProject/Controllers/BookController.cs
+++ BookingServices/CusromerProject/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BookingServices.Data;
 using Microsoft.EntityFrameworkCore;
+using CusromerProject.Models;
 
 namespace CusromerProject.Controllers
 {
@@ -10,26 +11,140 @@ namespace CusromerProject.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public BookController(ApplicationDbContext context) 
+        public BookController(ApplicationDbContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Booking>>> GetBookings()
+        public IActionResult GetBookings()
         {
-            return await _context.Bookings.ToListAsync();
+            List<Book> books = new List<Book>();
+            foreach (var item in _context.Bookings.Include(b => b.Reviews).ToList())
+            {
+                Book newBook = new Book()
+                {
+                    BookId = item.BookingId,
+                    BookDate = item.BookDate,
+                    CashOrCashByHandOrInstallment = item.CashOrCashByHandOrInstallment,
+                    CustomerId = item.CustomerId,
+                    EndTime = item.EndTime,
+                    EventDate = item.EventDate,
+                    PaymentIncomeId = item.PaymentIncomeId,
+                    Price = item.Price,
+                    Quantity = item.Quantity,
+                    Type = item.Type,
+                    StartTime = item.StartTime,
+                    Status = item.Status,
+                };
+                books.Add(newBook);
+            }
+            return Ok(books);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Booking>> GetBooking(int id)
+        public async Task<IActionResult> GetBooking(int id)
         {
-            var booking = await _context.Bookings.FindAsync(id);
-            if (booking == null)
+            var item = await _context.Bookings.Include(b => b.Reviews).Where(b => b.BookingId == id).FirstOrDefaultAsync();
+            if (item == null)
             {
-                return NotFound();
+                return NotFound("Not Found This Book");
             }
-            return booking;
+            Book newBook = new Book()
+            {
+                BookId = item.BookingId,
+                BookDate = item.BookDate,
+                CashOrCashByHandOrInstallment = item.CashOrCashByHandOrInstallment,
+                CustomerId = item.CustomerId,
+                EndTime = item.EndTime,
+                EventDate = item.EventDate,
+                PaymentIncomeId = item.PaymentIncomeId,
+                Price = item.Price,
+                Quantity = item.Quantity,
+                Type = item.Type,
+                StartTime = item.StartTime,
+                Status = item.Status,
+            };
+            return Ok(newBook);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBook(Book book)
+        {
+            if (book == null)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                Booking newBooking = new Booking()
+                {
+                    CashOrCashByHandOrInstallment = book.CashOrCashByHandOrInstallment,
+                    BookDate = book.BookDate,
+                    CustomerId = book.CustomerId,
+                    EndTime = book.EndTime,
+                    StartTime = book.StartTime,
+                    EventDate = book.EventDate,
+                    PaymentIncomeId = book.PaymentIncomeId,
+                    Price = book.Price,
+                    Status = "Pending",
+                    Quantity = book.Quantity,
+                    Type = book.Type,
+                };
+                _context.Bookings.Add(newBooking);
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    if (newBooking.Type == "Service")
+                    {
+                        BookingService newBookService = new BookingService()
+                        {
+                            BookingId = newBooking.BookingId,
+                            ServiceId = book.ServiceId,
+                        };
+                        _context.BookingServices.Add(newBookService);
+                        await _context.SaveChangesAsync();
+                    }
+                    else if (newBooking.Type == "Package") { }
+                    else if (newBooking.Type == "Consultation") { }
+                    return Ok("Added Successfully");
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var getBook = _context.Bookings.Find(id);
+            var getBookService = _context.BookingServices.Where(b => b.BookingId == id).FirstOrDefault();
+            if (getBook == null)
+                return NotFound("No Book With This ID");
+            if (getBookService == null)
+                return NotFound("No Book For Service With This ID");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.BookingServices.Remove(getBookService);
+                    _context.SaveChanges();
+                    _context.Bookings.Remove(getBook);
+                    _context.SaveChanges();
+                    return Ok($"Deleted Successfully");
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Error: {e.Message}");
+                }
+            }
+            return BadRequest();
         }
     }
 }
--- BookingServices/CusromerProject/Models/Book.cs
+++ BookingServices/CusromerProject/Models/Book.cs
@@ -1,10 +1,43 @@
-﻿namespace CusromerProject.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CusromerProject.Models
 {
     public class Book
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime BookingDate { get; set; }
-        public int Seats { get; set; }
+        public int BookId { get; set; }
+
+        [Required(ErrorMessage = "Event date is required.")]
+        public DateTime EventDate { get; set; }
+
+        [Required(ErrorMessage = "Start Time is required.")]
+        public TimeOnly StartTime { get; set; } // set start time and end time as string in API
+
+        [Required(ErrorMessage = "End Time is required.")]
+        public TimeOnly EndTime { get; set; }
+
+        [StringLength(20), JsonIgnore]
+        public string? Status { get; set; } // pending - paid - complete - canceled
+
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Range(1.0, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Payment method is required.")]
+        public required string CashOrCashByHandOrInstallment { get; set; }
+
+        public DateTime BookDate { get; set; } = DateTime.Now;
+
+        [StringLength(20)]
+        public string? Type { get; set; } //Package - Service - Consultation
+
+        public string? CustomerId { get; set; }
+
+        public int ServiceId { get; set; }
+
+        public int? PaymentIncomeId { get; set; }
     }
 }
