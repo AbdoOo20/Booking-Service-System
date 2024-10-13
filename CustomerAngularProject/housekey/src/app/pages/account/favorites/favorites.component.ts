43d3a516963b706a1240271a46ba598a58a8cfbd
MohamedShaban <m2.sha226@gmail.com>
2024-10-13 18:21:57 +0300

    Favorite Done

--- CustomerAngularProject/housekey/src/app/pages/account/favorites/favorites.component.ts
+++ CustomerAngularProject/housekey/src/app/pages/account/favorites/favorites.component.ts
@@ -1,10 +1,11 @@
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { WishlistService } from '../../../services/wishlist.service'; 
+import { WishlistService } from '@services/wishlist.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
@@ -12,7 +13,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
-
+import { log } from 'node:console';
+import { Property } from '@models/app.models';
 
 @Component({
   selector: 'app-favorites',
@@ -29,17 +31,17 @@ import { MatTable } from '@angular/material/table';
     MatSortModule,
   ]
 })
-  
 export class FavoritesComponent implements OnInit {
-  displayedColumns: string[] = ['name','location','price', 'image', 'actions'];
-  dataSource: MatTableDataSource<any>;
+  displayedColumns: string[] = ['name', 'location', 'price', 'image', 'actions'];
+  //dataSource: MatTableDataSource<any>;
+  dataSource: MatTableDataSource<Property>;
   @ViewChild(MatTable) table: MatTable<any>;
 
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private wishlistService: WishlistService) {}
+  constructor(private wishlistService: WishlistService) { }
 
   ngOnInit() {
     this.loadWishlist('customer-id'); // استخدم customerId الفعلي هنا
@@ -59,28 +61,45 @@ export class FavoritesComponent implements OnInit {
     );
   }
 
-  remove(property: any) {
+  public remove(property: Property) {
     const index: number = this.dataSource.data.indexOf(property);
-  
+
     if (index !== -1) {
+      this.dataSource.data.splice(index, 1);
+      this.dataSource = new MatTableDataSource<Property>(this.dataSource.data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      // استدعاء خدمة حذف العنصر
       this.wishlistService.deleteServiceFromWishlist('customer-id', property.id).subscribe(
         (response) => {
-          console.log(response);
-  
+          console.log('Service deleted successfully:', response);
+
           // إزالة العنصر من المصفوفة
-          this.dataSource.data = this.dataSource.data.filter(item => item !== property);
-  
+          //this.dataSource.data.splice(index, 1);
+
+          // تحديث الـ MatTableDataSource
+          // this.dataSource = new MatTableDataSource<Property>(this.dataSource.data);
+          // this.dataSource.paginator = this.paginator;
+          //this.dataSource.sort = this.sort;
+
           // تحديث الجدول بدون إعادة تحميل الصفحة
           this.table.renderRows();
-          window.location.reload()
         },
         (error) => {
-          console.error('Error deleting service', error);
+          console.error('Error deleting service:', error);
         }
       );
     }
   }
-  
+
+
+
+
+
+
+
+
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -90,17 +109,17 @@ export class FavoritesComponent implements OnInit {
   }
 }
 
-// @NgModule({
-//   imports: [
-//     CommonModule,
-//     RouterModule,
-//     MatFormFieldModule,
-//     MatInputModule,
-//     MatTableModule,
-//     MatPaginatorModule,
-//     MatIconModule,
-//     MatSortModule,
-//     FavoritesComponent // استيراد المكون هنا
-//   ]
-// })
-// export class FavoritesModule { }
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatSortModule,
+    FavoritesComponent // استيراد المكون هنا
+  ]
+})
+export class FavoritesModule { }
