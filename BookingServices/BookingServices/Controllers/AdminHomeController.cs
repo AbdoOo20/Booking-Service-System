a438d51a17afa24e5e6db9304d8f630aafa95bd3
mohamed <mohamedhosni19971997@gmail.com>
2024-10-14 22:17:54 +0300

    MVC Dashboard Updated

--- BookingServices/BookingServices/Controllers/AdminHomeController.cs
+++ BookingServices/BookingServices/Controllers/AdminHomeController.cs
@@ -26,14 +26,21 @@ namespace BookingServices.Controllers
             var services = context.Services.Count();
             var servicesOffLine = context.Services.Where(s => s.IsOnlineOrOffline == false).Count();
             var servicesOnLine = context.Services.Where(s => s.IsOnlineOrOffline == true).Count();
-            var totalMoneyService = context.Payments.Where(p=>p.Booking.Type == "Service").Sum(p=>p.PaymentValue);
+            var totalMoneyService = context.Bookings
+                .Join(context.BookingServices, b => b.BookingId, bs => bs.BookingId, (b, bs) => new { b, bs })
+                .Join(context.Services, b_bs => b_bs.bs.ServiceId, s => s.ServiceId, (b_bs, s) => new { b_bs.b, s })
+                .Join(context.PaymentIncomes, b_s => b_s.b.PaymentIncomeId, p => p.PaymentIncomeId, (b_s, p) => new { b_s.b, b_s.s, p })
+                .Where(joined => joined.b.Status == "Confirmed")
+                .Select(joined => (joined.b.Price * joined.p.Percentage / 100))
+                .Sum();
+            string formattedValue = totalMoneyService.ToString("F2");
             HomeAdminVM homeAdminVM = new HomeAdminVM();
             homeAdminVM.CustomerCount = customers;
             homeAdminVM.ProvidersCount = providers;
             homeAdminVM.ServicesCount = services;
             homeAdminVM.ServicesOffLineCount = servicesOffLine;
             homeAdminVM.ServicesOnLineCount = servicesOnLine;
-            homeAdminVM.TotalMoneyService = totalMoneyService;
+            homeAdminVM.TotalMoneyService = formattedValue;
             //Start Chart
             var salesData = new List<int> { providers , customers };
             ViewBag.SalesData = salesData;
